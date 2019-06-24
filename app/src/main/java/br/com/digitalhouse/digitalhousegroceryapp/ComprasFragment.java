0e831a86f76965320472ae0dfe404c00b8016b05
michael <michaelpinheiro00@gmail.com>
2019-06-24 11:22:00 -0300

    Lista Recycler View

--- app/src/main/java/br/com/digitalhouse/digitalhousegroceryapp/ComprasFragment.java
+++ app/src/main/java/br/com/digitalhouse/digitalhousegroceryapp/ComprasFragment.java
@@ -3,9 +3,17 @@ package br.com.digitalhouse.digitalhousegroceryapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.digitalhouse.digitalhousegroceryapp.adapter.ProdutoAdapter;
+import br.com.digitalhouse.digitalhousegroceryapp.model.Produto;
 
 
 /**
@@ -22,8 +30,39 @@ public class ComprasFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment1, container, false);
+
+        List<Produto> listaProdutos = getListaProdutos();
+
+        ProdutoAdapter produtoAdapter = new ProdutoAdapter(listaProdutos);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(view.getContext());
+
+        RecyclerView recyclerView = view.findViewById(R.id.produtos_recycler_view);
+        recyclerView.setAdapter(produtoAdapter);
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
+    }
+
+    private List<Produto> getListaProdutos (){
+        List<Produto> listaProdutos = new ArrayList<>();
+        Produto produto1 = new Produto("Cebola",2,"kg",false);
+        listaProdutos.add(produto1);
+
+        Produto produto2 = new Produto("Arroz",5,"kg",false);
+        listaProdutos.add(produto2);
+
+        Produto produto3 = new Produto("Bolacha",2,"un",false);
+        listaProdutos.add(produto3);
+
+        Produto produto4 = new Produto("Batata",1,"kg",true);
+        listaProdutos.add(produto4);
+
+        Produto produto5 = new Produto("Papel Higiênico",2,"un",false);
+        listaProdutos.add(produto5);
+
+        return listaProdutos;
     }
 
 }
---
+++ app/src/main/java/br/com/digitalhouse/digitalhousegroceryapp/model/Produto.java
@@ -0,0 +1,50 @@
+package br.com.digitalhouse.digitalhousegroceryapp.model;
+
+public class Produto {
+
+    private String descricao;
+    private float quantidade;
+    private String unidade;
+    private boolean comprado;
+
+
+
+    public Produto(String descricao, float quantidade, String unidade, boolean comprado) {
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.unidade = unidade;
+        this.comprado = comprado;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(float quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public boolean isComprado() {
+        return comprado;
+    }
+
+    public void setComprado(boolean comprado) {
+        this.comprado = comprado;
+    }
+}
